7ec1106e9ed88f127a6cd25f02d09ad90d324557
Helen Yitbarek <helen.yitbarekk22@gmail.com>
2025-08-13 21:17:54 +0300

    Create Your Magical Table correction on the author

--- task_2.sql
+++ task_2.sql
@@ -1,14 +1,13 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE alx_book_store;
-
-CREATE TABLE authors (
+CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
+    author_name VARCHAR(255) NOT NULL,
     birth_date DATE,
     country VARCHAR(100)
 );
+
 
 
 CREATE TABLE books (
